aa10864f541deb984f463cc851bd3ffc1ce8ac23
bhayek12@gmail.com <bhayek12@gmail.com>
2019-05-10 15:09:22 -0600

    Add update document function

--- app.js
+++ app.js
@@ -41,10 +41,23 @@ const findDocuments = function(db, callback) {
   // Get the documents collection
   const collection = db.collection('documents');
   // Find some documents
-  collection.find({}).toArray(function(err, docs) {
+  collection.find({'a': 3}).toArray(function(err, docs) {
     assert.equal(err, null);
     console.log("Found the following records");
-    console.log(docs)
+    console.log(docs);
     callback(docs);
   });
+}
+
+const updateDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });  
 }
