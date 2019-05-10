31ab2f7f0449a084d59a3d4fc56f9b625a5ff7a0
bhayek12@gmail.com <bhayek12@gmail.com>
2019-05-10 15:07:19 -0600

    add funtion to run

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const url = 'mongodb://localhost:27017';
 
 // Database Name
 const dbName = 'myproject';
-
-// Create a new MongoClient
 const client = new MongoClient(url);
 
-// Use connect method to connect to the Server
+// Use connect method to connect to the server
 client.connect(function(err) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
 
-  client.close();
+  insertDocuments(db, function() {
+    client.close();
+  });
 });
 
 const insertDocuments = function(db, callback) {
